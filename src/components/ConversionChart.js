45e8cd3fef0fe979ceb2468b51f288610c624304
Gage Clancy <gclancy121@gmail.com>
2022-11-29 14:29:46 -0600

    more setup on conversion menu setup

--- src/components/ConversionChart.js
+++ src/components/ConversionChart.js
@@ -1,9 +1,37 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
 
 function ConversionChart() {
+    const URL = 'http://localhost:9000';
+
+    const initialFormValues = {
+        usd: '',
+        conversion_value: ''
+    }
+    const [conversions, setConversions] = useState([]);
+    const [formValues, setFormValues] = useState(initialFormValues);
+
+    function handleChange(evt) {
+        setFormValues({
+            ...formValues,
+            [evt.target.name]: evt.target.value
+        });
+    }
+
+    useEffect(() => {
+        axios.get(`${URL}/conversions`).then(res => setConversions(res.data));
+    }, [])
     return (
         <div>
-            Chart Begins here!
+           <form>
+                <label>United States Dollars</label>
+                <input type='text' name='usd' onChange={handleChange}/>
+                <label>Convert To</label>
+                <select value={formValues.conversion_value} name='conversion_value' onChange={handleChange}>
+                    <option value=''>---Select a Country---</option>
+                    {conversions.map(conversion => <option value={conversion.conversion_rate}>{conversion.country_name}</option>)}
+                </select>
+           </form>
         </div>
     )
 }
