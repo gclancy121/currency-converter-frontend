56512607c7c10caf2076de422b785313d56409c1
Gage Clancy <gclancy121@gmail.com>
2022-11-29 15:27:50 -0600

    added program to fetch currency name

---
+++ src/components/currencyName.js
@@ -0,0 +1,66 @@
+function findCurrencyName(conversion) {
+    if (conversion === '0.727') {
+        return 'British pound'
+    } else if (conversion === '0.845') {
+        return 'Euro'
+    } else if (conversion === '0.914') {
+        return 'Swiss franc'
+    } else if (conversion === '1.254') {
+        return 'Canadian dollar'
+    } else if (conversion === '1.331') {
+        return "Australian dollar"
+    } else if (conversion === '1.343') {
+        return 'Singapore dollar'
+    } else if (conversion === '1.414') {
+        return 'New Zealand dollar'
+    } else if (conversion === '1.654') {
+        return 'Bulgarian lev'
+    } else if (conversion === '3.222') {
+        return 'Georgian lari'
+    } else if (conversion === '3.23') {
+        return "Israeli shekel"
+    } else if (conversion === '3.75') {
+        return 'Saudi riyal'
+    } else if (conversion === '3.862') {
+        return 'Polish złoty'
+    } else if(conversion === '4.16') {
+        return "Romanian leu"
+    } else if(conversion === '5.394') {
+        return 'Brazilian Real'
+    } else if(conversion === '6.287') {
+        return "Danish krone"
+    } else if(conversion === '6.36') {
+        return "Croatian kuna"
+    } else if(conversion === '6.449') {
+        return 'Chinese yuan'
+    } else if(conversion === '7.773') {
+        return 'Hong Kong dollar'
+    } else if(conversion === '8.577') {
+        return 'Swedish krona'
+    } else if(conversion === '8.59') {
+        return 'Norwegian krone'
+    } else if(conversion === '8.85') {
+        return 'Turkish lira'
+    } else if(conversion === '8.988') {
+        return 'Moroccan dirham'
+    } else if(conversion === '14.779') {
+        return 'South African rand'
+    } else if(conversion === '20.018') {
+        return 'Zambian kwacha'
+    } else if(conversion === '20.272') {
+        return 'Mexican peso'
+    } else if(conversion === '21.678') {
+        return 'Czech koruna'
+    } else if(conversion === '52.102') {
+        return 'Macedonian Denar'
+    } else if(conversion === '73.654') {
+        return "Russian rouble"
+    } else if (conversion === '73.918') {
+        return "Indian rupee"
+    } else if(conversion === "94.918") {
+        return "Argentinian peso"
+    } else if(conversion === '99.531') {
+        return "Serbian dinar"
+    } else if(conversion === '103.52') {
+        return 'Albanian lek'
+    } else if(conversion === )
