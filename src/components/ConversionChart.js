5d1b3c83a396055993f95a3e61eef3a654ccbdf4
Gage Clancy <gclancy121@gmail.com>
2022-11-29 14:41:55 -0600

    bare bones application completed

--- src/components/ConversionChart.js
+++ src/components/ConversionChart.js
@@ -10,6 +10,7 @@ function ConversionChart() {
     }
     const [conversions, setConversions] = useState([]);
     const [formValues, setFormValues] = useState(initialFormValues);
+    const [converted, setConverted] = useState('');
 
     function handleChange(evt) {
         setFormValues({
@@ -18,12 +19,35 @@ function ConversionChart() {
         });
     }
 
+    const onSubmit = evt => {
+        evt.preventDefault();
+        const mathData = {
+            usd: formValues.usd,
+            conversion_value: formValues.conversion_value
+        }
+        const maths = mathData.usd * mathData.conversion_value;
+        setConverted(maths);
+    }
+
+    function FinalNumbers() {
+        if (converted === '') {
+            return (
+                <></>
+            )
+        } else {
+            return (
+                <div className='conversions'>
+                    <h4>{`${converted} ` }</h4>
+                </div>
+            )
+        }
+    } 
     useEffect(() => {
         axios.get(`${URL}/conversions`).then(res => setConversions(res.data));
     }, [])
     return (
         <div>
-           <form>
+           <form onSubmit={onSubmit}>
                 <label>United States Dollars</label>
                 <input type='text' name='usd' onChange={handleChange}/>
                 <label>Convert To</label>
@@ -31,7 +55,9 @@ function ConversionChart() {
                     <option value=''>---Select a Country---</option>
                     {conversions.map(conversion => <option value={conversion.conversion_rate}>{conversion.country_name}</option>)}
                 </select>
+                <input type='submit' />
            </form>
+           <FinalNumbers />
         </div>
     )
 }
